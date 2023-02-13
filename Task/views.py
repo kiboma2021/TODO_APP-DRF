b6c43a297617f702a8d8c5f6a9a43b1341a4b845
kiboma <omayioben@gmail.com>
2023-02-13 08:36:54 +0300

    Add view to get serialized data

--- Task/views.py
+++ Task/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
-
+from .serializers import TaskSerializer
+from .models import Task
+from rest_framework.response import Response
+from rest_framework import status
+from rest_framework.decorators import api_view
 # Create your views here.
+
+@api_view(['GET', 'POST'])
+def TaskAPI(request):
+    try:
+        tasks=Task.objects.all()
+    except Task.DoesNotExist:
+        return Response(status=status.HTTP_400_BAD_REQUEST)
+
+    if request.method == 'GET':
+        serialized_data = TaskSerializer(tasks, many=True)
+        return Response(serialized_data.data, status=status.HTTP_200_OK)
